bc21de1e42109b28560f1e3e0d98cdf4f372204a
Tomislav Dukez <tomdu3@ymail.com>
2024-06-09 06:44:27 +0200

    add account app and default user model

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "account",
+    "server",
 ]
 
 MIDDLEWARE = [
@@ -114,3 +116,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom user model
+AUTH_USER_MODEL = "account.Account"
