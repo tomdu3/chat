855986ed7768e3dde5ffcef5b5dfe5653dd5bed5
Tomislav Dukez <tomdu3@ymail.com>
2024-06-13 16:52:47 +0200

    add default authentication session class

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -126,6 +126,9 @@ AUTH_USER_MODEL = "account.Account"
 # Rest framework settings
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_frameword.authentication.SessionAuthentication",
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
