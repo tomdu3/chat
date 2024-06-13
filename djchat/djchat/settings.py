199c917c2e7e330d8d3c0dcdbc16cb7c1d33b1a2
Tomislav Dukez <tomdu3@ymail.com>
2024-06-13 17:44:50 +0200

    fix mispelled framework in settings

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -127,7 +127,7 @@ AUTH_USER_MODEL = "account.Account"
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_frameword.authentication.SessionAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
 }
 
