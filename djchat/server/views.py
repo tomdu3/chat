14b2cc5994f563b638f681b70f1d59bc8302636f
Tomislav Dukez <tomdu3@ymail.com>
2024-06-14 20:16:09 +0200

    add filter servers by quantity

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -7,14 +7,21 @@ from .serializers import ServerSerializer
 
 
 class ServerListViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing and filtering servers.
+    """
 
     queryset = Server.objects.all()
 
     def list(self, request):
         category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
+
+        if qty:
+            self.queryset = self.queryset.order_by("-id")[: int(qty)]
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
