4c575e4ead799b20ff7a3ffdaaf0f1e83b1dc22d
Tomislav Dukez <tomdu3@ymail.com>
2024-06-15 11:54:11 +0200

    add filter sever by serverid and error handling user authentication

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
 from .models import Server
@@ -17,6 +18,10 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
+        by_serverid = request.query_params.get("by_serverid")
+
+        if by_user or by_serverid and not request.user.is_authenticated:
+            raise AuthenticationFailed(detail="User not authenticated")
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
@@ -27,6 +32,16 @@ class ServerListViewSet(viewsets.ViewSet):
 
         if qty:
             self.queryset = self.queryset.order_by("-id")[: int(qty)]
+
+        if by_serverid:
+            try:  # it can break if by_serverid is not an int
+                self.queryset = self.queryset.filter(id=by_serverid)
+                if not self.queryset.exists():
+                    raise ValidationError(
+                        detail=f"Server with id {by_serverid} not found"
+                    )
+            except ValueError:
+                raise ValidationError(detail=f"Server value error")
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
