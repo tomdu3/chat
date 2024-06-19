c7171c5a6e07daafbad3aa70472f2fd6fb46f016
Tomislav Dukez <tomdu3@ymail.com>
2024-06-19 21:04:59 +0200

    add image and static settings

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 from server.views import ServerListViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 # automated routers for server
 router = DefaultRouter()
@@ -14,3 +16,6 @@ urlpatterns = [
     path("api/docs/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/schema/ui", SpectacularSwaggerView.as_view()),
 ] + router.urls
+
+if settings.DEUBG:
+    urlpattern += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
