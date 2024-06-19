1d1efbccb14a17eff2be064c3b9814c45ba4eb26
Tomislav Dukez <tomdu3@ymail.com>
2024-06-19 21:34:10 +0200

    add icon image to model

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -2,9 +2,18 @@ from django.conf import settings
 from django.db import models
 
 
+def category_icon_upload_path(instance, filename):
+    return f"category/{instance.id}/category_icon/{filename}"
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(
+        upload_to=category_icon_upload_path,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.name
