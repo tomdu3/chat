bda4edfd7397cfec4fb1eadda658d0c04bc8e4cb
Tomislav Dukez <tomdu3@ymail.com>
2024-06-25 09:50:40 +0200

    add home route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
-function App() {
-  return <></>;
-}
+import Home from "./pages/Home";
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+    </Route>,
+  ),
+);
+
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
